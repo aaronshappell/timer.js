a8f57f72088e67bd6c733d4f54b0006cabccfec7
aaronshappell <crumster88@gmail.com>
2017-11-22 19:07:59 -0800

    Add multiplier

--- timer.js
+++ timer.js
@@ -4,7 +4,7 @@
  */
 
 /**
- * Defines a timer object
+ * Defines a timer object with a multiplier of 1
  * @constructor
  */
 var Timer = function(){
@@ -13,6 +13,7 @@ var Timer = function(){
     this.lastTime = 0;
     this.timePaused = 0;
     this.running = false;
+    this.multiplier = 1;
 }
 
 /**
@@ -88,4 +89,44 @@ Timer.prototype.getDeltaTime = function(){
     var delta = Date.now() - this.lastTime;
     this.lastTime = Date.now();
     return delta;
+}
+
+/**
+ * 
+ * @return {number} the multiplied elasped time in milliseconds
+ */
+Timer.prototype.getMultipliedElapsedMillis = function(){
+    return this.getElapsedMillis() * this.multiplier;
+}
+
+/**
+ * Gets the multiplied elapsed time of the timer in seconds
+ * @return {number} the multiplied elasped time in seconds
+ */
+Timer.prototype.getMultipliedElapsedSeconds = function(){
+    return this.getElapsedSeconds() * this.multiplier;
+}
+
+/**
+ * Gets the multiplied delta time of the timer
+ * @return {number} the multiplied delta time
+ */
+Timer.prototype.getMultipliedDeltaTime = function(){
+    return this.getDeltaTime() * this.multiplier;
+}
+
+/**
+ * Sets the timer multiplier
+ * @param {number} multiplier the multiplier to be set
+ */
+Timer.prototype.setMultiplier = function(multiplier){
+    this.multiplier = multiplier;
+}
+
+/**
+ * Gets the timer multiplier
+ * @return {number} the multiplier
+ */
+Timer.prototype.getMultiplier = function(){
+    return this.multiplier;
 }
